142a33698de8f609856299300f7e22ae2f3de668
jahuey9212 <jahuey9212@gmail.com>
2021-08-18 12:55:53 -0500

    Fin

--- public/index.php
+++ public/index.php
@@ -3,6 +3,16 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
+
 define('LARAVEL_START', microtime(true));
 
 /*
